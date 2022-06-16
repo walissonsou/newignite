ab2dba83818c62ee9bbc95dff4cdb68f8248a9c2
walissonsou <walissonsouza96@hotmail.com>
2022-06-16 12:22:14 -0300

    commits iniciais e add header

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <p>OI OI PAPAI</p>
+      <p>
+       
+          Learn Preact
+       
+      </p>
+    </>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+import igniteLogo from '../assets/logo.svg';
+
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+    <img src={igniteLogo} alt="Logotipo do Ignite" />
+      
+    </header>
+  );
+}
