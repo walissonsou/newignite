88c2a221ac6040b2e6deac9068259c54da1dcb20
walissonsou <walissonsouza96@hotmail.com>
2022-06-16 13:36:22 -0300

    finalizando sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import styles from './SideBar.module.css'
+import React from 'react';
+import styles from './SideBar.module.css';
+import { Phone } from "phosphor-react"
 export default function SideBar() {
   return (
     <aside className={styles.sidebar}>
-      Oi oi papai
+      <img className={styles.cover} 
+      src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      alt="wallpapper"
+
+      />
+      <div className={styles.profile}> 
+        <img  src="https://avatars.githubusercontent.com/u/88872797?v=4" alt="" className={styles.avatar}></img>
+        <strong> Walisson Souza </strong>
+        <span> Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <Phone size={20}/>
+          Contate-me
+        </a>
+      </footer>
     </aside>
   )
 }
