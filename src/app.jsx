eb12744e50aae409afbc80092fb64ffe3e097996
walissonsou <walissonsouza96@hotmail.com>
2022-07-12 23:56:10 -0300

    primeira iteração ( iterando JSX)

--- src/app.jsx
+++ src/app.jsx
@@ -4,17 +4,57 @@ import styles from './App.module.css'
 import './global.css';
 import SideBar from './components/SideBar/SideBar';
 
-export function App() {
+
+// author: { avatar_url: "", name: "", role: "",}
+// publishedAt: Date 
+// content: string
+
+const posts = [ 
+  {
+    id: 1,
+    author:{
+      avatar_url: 'https://github.com/walissonsou.png',
+      name: 'Walisson Souza' ,
+      role: "Dev Front GCT ",
+ },
+ content: [
+  {type: 'paragraph', content: "oi oi papai"}
+ ],
+ publishedAt: new Date('2022-06-18 22:00:00')
+  },
+  {
+    id: 2,
+    author:{
+      avatar_url: 'https://github.com/walissonsou.png',
+      name: 'Walisson Souza' ,
+      role: "Dev Front GCT ",
+ },
+ content: [
+  {type: 'paragraph', content: "oi oi papai"}
+ ],
+ publishedAt: new Date('2022-06-18 22:00:00')
+  },
+ 
+];
+
+export function App(props) {
+  console.log(props);
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
           <SideBar />
           <main>
-            <Post
+          {posts.map(post => {
+            return (
+              <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
             />
-             <Post
-             /> 
+            )
+            
+          })}
           </main>
       </div>
     </div>
